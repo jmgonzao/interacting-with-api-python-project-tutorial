ca8d8b9eec60bcb4dac412db83eb217ec751a9d5
jmgonzao <jmgonzalezo@outlook.com>
2025-07-01 01:53:21 +0000

    Taller API

--- src/app.py
+++ src/app.py
@@ -1,10 +1,38 @@
 import os
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
 
 # Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# API Request
+artist_id = "3TVXtAsR1Inumwj472S9r4"
+
+# Get the top tracks of an artist
+results = spotify.artist_top_tracks(artist_id)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
+
+# Transformar en DataFrame
+
+df = pd.DataFrame(songs)
